f333d7c4da63c0a3dc3befb0b648af1960ed4c6f
EsGarciaC <80054233+EsGarciaC@users.noreply.github.com>
2021-10-15 17:48:32 -0500

    Added Asiento, Auto and Motor

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Auto {
+
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	public int cantidadAsientos() {
+		int x = 0;
+		for(int i = 0; i < asientos.length; i++){
+			if (asientos[i] instanceof Asiento) {
+				x++;
+			}
+		}
+		return x;
+	}
+	
+	public String verificarIntegridad() {
+		for(int i = 0; i < asientos.length; i++) {
+			if (registro != asientos[i].registro && registro != motor.registro) {
+				return "Las piesas no son originales";
+			}
+		}
+		return "Auto original";
+	}
+}
